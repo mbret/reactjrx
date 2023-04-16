92cd6b629ef8bfda334efb0fd6e84ddaf7f9e8f1
maxime <bret.maxime@gmail.com>
2023-04-16 15:19:04 +0900

    fix: fixed deps build

--- vite.config.ts
+++ vite.config.ts
@@ -18,14 +18,7 @@ export default defineConfig({
     rollupOptions: {
       // make sure to externalize deps that shouldn't be bundled
       // into your library
-      external: ["vue", "rxjs", "react"],
-      output: {
-        // Provide global variables to use in the UMD build
-        // for externalized deps
-        globals: {
-          vue: "Vue",
-        },
-      },
+      external: ["rxjs", "rxjs/operators", "react"],
     },
   },
 });
