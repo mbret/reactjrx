4c818a61dc811d0366486278754643980860b78a
maxime <bret.maxime@gmail.com>
2023-04-22 05:40:22 +0900

    feat: lint

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,27 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
-import { useQuery } from "./lib/queries/useQuery"
-import { interval, of, take, timer } from "rxjs"
-
-const s = interval(100).pipe(take(5))
+import {
+  interval,
+  map,
+  tap
+} from "rxjs"
+import { useSubscribe } from "./lib/useSubscribe"
+import { useObserve } from "./lib/useObserve"
 
 const App = () => {
-  console.log(
-    "FOOOO",
-    useQuery(() => timer(10))
+  useObserve(() => interval(1000), [])
+  useSubscribe(
+    () =>
+      interval(300).pipe(
+        map((v) => {
+          if (v > 1) {
+            throw new Error("")
+          }
+          return v
+        }),
+        tap(console.log)
+      ),
+    []
   )
 
   return null
