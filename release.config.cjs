0100d230666194f47348918dc289a515dc190019
maxime <bret.maxime@gmail.com>
2023-05-06 09:59:12 +0200

    fix: oupsie

--- release.config.cjs
+++ release.config.cjs
@@ -2,12 +2,15 @@ module.exports = {
   branches: ["main"],
   plugins: [
     [
+      "@semantic-release/commit-analyzer",
+      "@semantic-release/release-notes-generator",
       // https://github.com/semantic-release/semantic-release/issues/2204#issuecomment-1486299917
       "@semantic-release/github",
       {
-        // successComment: false,
-        // failTitle: false
-      }
+        successComment: false,
+        failTitle: false
+      },
+      "@semantic-release/npm"
     ]
   ]
 }
