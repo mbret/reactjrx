d52341d3929a5c5b742648fcf408bb1ae859f253
maxime <bret.maxime@gmail.com>
2023-06-12 22:25:29 +0200

    feat: updated main playground

--- src/main.tsx
+++ src/main.tsx
@@ -1,26 +1,61 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import ReactDOM from "react-dom/client"
-import { interval, map, tap } from "rxjs"
-import { useObserve } from "./lib/binding/useObserve"
-import { useSubscribe } from "./lib/binding/useSubscribe"
+import { useMutation } from "./lib/queries/useMutation"
 
-const App = () => {
-  useObserve(() => interval(1000), [])
-  useSubscribe(
-    () =>
-      interval(300).pipe(
-        map((v) => {
-          if (v > 1) {
-            throw new Error("")
-          }
-          return v
-        }),
-        tap(console.log)
-      ),
-    []
+let index = 0
+
+const Effects = () => {
+  const { mutate, reset, data } = useMutation(
+    async (arg: string) => {
+      index++
+
+      console.log("run", arg)
+
+      await new Promise<void>((resolve) => {
+        setTimeout(() => {
+          resolve()
+        }, 500)
+      })
+
+      return arg
+    },
+    {
+      cancelOnUnMount: false,
+      onSuccess: (arg) => {
+        console.log("mutated", arg)
+      }
+    }
   )
 
+  console.log({ data })
+
+  useEffect(() => {
+    mutate(`mount-${index}`)
+
+    setTimeout(() => {
+      reset()
+      mutate(`timeout-${index}`)
+    }, 499)
+
+    return () => {
+      console.log("unmounted")
+      reset()
+    }
+  }, [mutate, reset])
+
   return null
+}
+
+const App = () => {
+  const [visible, setVisible] = useState(true)
+
+  useEffect(() => {
+    setTimeout(() => {
+      setVisible(false)
+    }, 10)
+  }, [])
+
+  return visible ? <Effects /> : null
 }
 
 ReactDOM.createRoot(document.getElementById("app") as HTMLElement).render(
