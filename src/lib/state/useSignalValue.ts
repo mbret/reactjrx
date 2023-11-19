81adff10d113ae0764d1eaf82f954d56c20cd5b5
maxime <bret.maxime@gmail.com>
2023-11-19 10:47:39 +0100

    feat: added selector on useSignal

--- src/lib/state/useSignalValue.ts
+++ src/lib/state/useSignalValue.ts
@@ -1,6 +1,27 @@
+import { distinctUntilChanged, map } from "rxjs"
 import { useObserve } from "../binding/useObserve"
 import { type Signal } from "./signal"
 
-export const useSignalValue = <S, K>(signal: Signal<S, S, K>, key?: string) => {
-  return useObserve(signal.subject, { defaultValue: signal.getValue(), key })
+export function useSignalValue<S, K, L>(
+  signal: Signal<S, S, K>,
+  selector: (value: S) => L
+): L
+export function useSignalValue<S, K>(signal: Signal<S, S, K>): S
+export function useSignalValue<S, K, L>(
+  signal: Signal<S, S, K>,
+  selector?: (value: S) => L
+) {
+  const selectorOrDefault = selector ?? ((v) => v)
+
+  return useObserve(
+    () =>
+      signal.subject.pipe(
+        map((value) => selectorOrDefault(value)),
+        distinctUntilChanged()
+      ),
+    {
+      defaultValue: selectorOrDefault(signal.getValue())
+    },
+    []
+  )
 }
