5eae980877041de369d47455a84e68d556f8cd82
maxime <bret.maxime@gmail.com>
2024-02-09 19:22:31 +0100

    feat: added more helpers

---
+++ src/lib/binding/useObservableEvent.ts
@@ -0,0 +1,22 @@
+import { useCallback } from "react"
+import { useSubject } from "./useSubject"
+import { useLiveRef } from "../utils/useLiveRef"
+
+/**
+ * This creates an event handler and an observable that represents calls to that handler.
+ */
+export const useObserveCallback = <T = void>() => {
+  const subject = useSubject<T>()
+
+  const event$ = useLiveRef(subject.current.asObservable())
+
+  const trigger = useCallback(
+    (arg: T) => {
+      subject.current.next(arg)
+    },
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    []
+  )
+
+  return [event$, trigger] as const
+}
