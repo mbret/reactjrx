7aa14b906464a782ad0a2e982dee00f256ebfd6e
mbret <bret.maxime@gmail.com>
2025-03-30 00:35:47 +0100

    feat: added constant export

--- src/lib/utils/index.ts
+++ src/lib/utils/index.ts
@@ -2,6 +2,7 @@ export * from "./react/useUnmountObservable"
 export * from "./react/useMount"
 export * from "./react/useEffectOnce"
 export * from "./react/useLiveRef"
+export * from "./react/useConstant"
 
 export * from "./operators/retryBackoff"
 
@@ -9,3 +10,4 @@ export * from "./isDefined"
 export * from "./arrayEqual"
 export * from "./isServer"
 export * from "./isPromiseLike"
+export * from "./deepEqual"
